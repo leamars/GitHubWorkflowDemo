b8681fbb41c6e44e360dec5502634af4a67b83d5
Lea Marolt Sonnenschein <maroltso@grinnell.edu>
2018-05-26 11:43:11 -0400

    Add worfklow demo.

---
+++ GitHubWorkflowDemo/GitHubWorkflowDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubWorkflowDemo
+//
+//  Created by Lea Marolt Sonnenschein on 5/26/18.
+//  Copyright © 2018 elemes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
